dc2c082dcc38706d6dea38cd17906b11cdaf2aeb
brianp1010 <brianpineda101000@gmail.com>
2022-10-15 21:50:42 -0400

    Commit fork on brian's branch, UI will be updated soon.

--- forensic-app/screens/SearchScreen.js
+++ forensic-app/screens/SearchScreen.js
@@ -1,10 +1,52 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, FlatList } from "react-native";
+
+const initialArticlesData = [
+  { title: "Devin" },
+  { title: "Dan" },
+  { title: "Dominic" },
+  { title: "John" },
+  { title: "James" },
+  { title: "Julie" },
+];
 
 export default function SearchScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-            <Text style={{ fontSize: 16, fontWeight: '700' }}> Search Screen </Text>
-        </View>
+  const [searchText, setSearchText] = useState("");
+  const [articlesData, setArticlesData] = useState(initialArticlesData);
+
+  function handleSearchInput(prompt) {
+    console.log(prompt);
+    setSearchText(prompt);
+  }
+
+  function handleSearchButton() {
+    // call api here
+    const filtered = initialArticlesData.filter((article) =>
+      article.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
     );
+    console.log(filtered);
+    setArticlesData(filtered);
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+      }}
+    >
+      <TextInput
+        style={{ height: 40, width: 100, borderBottomColor: "black" }}
+        placeholder="Search"
+        onChangeText={handleSearchInput}
+        defaultValue={searchText}
+      />
+      <Button title="Search" onPress={handleSearchButton} />
+      <FlatList
+        data={articlesData}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
+    </View>
+  );
 }
